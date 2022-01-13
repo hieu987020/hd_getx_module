a6f95e80af535047d19f7fc0585eeb3d401f6e76
HieuHD <hieu987020@gmail.com>
2022-01-13 19:30:52 +0700

    update

--- lib/app/modules/home/controllers/home_controller.dart
+++ lib/app/modules/home/controllers/home_controller.dart
@@ -1,17 +1,6 @@
 import 'package:get/get.dart';
-import 'package:hd_getx_module/app/modules/home/views/home_widget.dart';
 
 class HomeController extends GetxController {
-  @override
-  void onInit() {
-    super.onInit();
-  }
-
-  @override
-  void onReady() {
-    super.onReady();
-  }
-
   @override
   void onClose() {}
 }
