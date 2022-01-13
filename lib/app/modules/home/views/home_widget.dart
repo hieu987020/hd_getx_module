53ac37f9032c4d98faea87cb20e3693c1c98d567
HieuHD <hieu987020@gmail.com>
2022-01-13 18:02:03 +0700

    update

--- lib/app/modules/home/views/home_widget.dart
+++ lib/app/modules/home/views/home_widget.dart
@@ -8,16 +8,40 @@ enum CustomeStyle {
 class CustomeWidget extends StatelessWidget {
   const CustomeWidget({
     Key? key,
+    this.width,
+    this.height,
     this.firstChild,
     this.secondChild,
     this.customeStyle = CustomeStyle.row,
     this.icon,
+    this.alignment,
+    this.padding,
+    this.color,
+    this.decoration,
+    this.foregroundDecoration,
+    this.constraints,
+    this.margin,
+    this.transform,
+    this.transformAlignment,
+    this.clipBehavior = Clip.none,
   }) : super(key: key);
 
+  final double? width;
+  final double? height;
   final Widget? firstChild;
   final Widget? secondChild;
   final CustomeStyle customeStyle;
   final Widget? icon;
+  final AlignmentGeometry? alignment;
+  final EdgeInsetsGeometry? padding;
+  final Color? color;
+  final Decoration? decoration;
+  final Decoration? foregroundDecoration;
+  final BoxConstraints? constraints;
+  final EdgeInsetsGeometry? margin;
+  final Matrix4? transform;
+  final AlignmentGeometry? transformAlignment;
+  final Clip clipBehavior;
 
   Widget _buildStyle() {
     if (customeStyle == CustomeStyle.row) {
