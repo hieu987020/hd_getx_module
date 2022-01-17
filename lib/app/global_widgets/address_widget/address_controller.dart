7cd61897ffd7fd3dde7c8273583a6b51d17284f1
HieuHD <hieu987020@gmail.com>
2022-01-18 01:49:07 +0700

    update

--- lib/app/global_widgets/address_widget/address_controller.dart
+++ lib/app/global_widgets/address_widget/address_controller.dart
@@ -3,6 +3,7 @@ import 'package:hd_getx_module/app/global_widgets/address_widget/address_widget.
 
 class AddressController extends GetxController {
   final addressStyle = AddressStyle.oneColumn.obs;
+
   void changeStyle() {
     if (addressStyle.value == AddressStyle.oneColumn) {
       addressStyle(AddressStyle.twoColumn);
--- lib/app/modules/home/views/home_view.dart
+++ lib/app/modules/home/views/home_view.dart
@@ -2,52 +2,63 @@ import 'package:flutter/material.dart';
 import 'package:get/state_manager.dart';
 import 'package:hd_getx_module/app/global_widgets/address_widget/address_widget.dart';
 import 'package:hd_getx_module/app/modules/home/controllers/home_controller.dart';
-import 'package:hd_getx_module/app/modules/home/local_widgets/my_child.dart';
+import 'package:hd_getx_module/app/modules/home/local_widgets/color_containers/my_child.dart';
 
 // ignore: use_key_in_widget_constructors
 class HomeView extends GetView<HomeController> {
+  List<Widget> buildChild1() {
+    return const [
+      MyChild(text: 'Child 1', color: Colors.green),
+      MyChild(text: 'Child 2', color: Colors.blue),
+      MyChild(text: 'Child 3', color: Colors.red),
+      MyChild(text: 'Child 4', color: Colors.yellow),
+      MyChild(text: 'Child 5', color: Colors.pinkAccent),
+      MyChild(text: 'Child 6', color: Colors.lightGreen),
+    ];
+  }
+
+  List<Widget> buildChild2() {
+    return const [
+      MyChild(text: 'Child 1', color: Colors.green),
+      MyChild(text: 'Child 2', color: Colors.blue),
+      MyChild(text: 'Child 3', color: Colors.red),
+      MyChild(text: 'Child 4', color: Colors.yellow),
+    ];
+  }
+
+  Widget buildTitle() {
+    return const MyChild(
+        width: 200, text: 'THIS IS TITLE', color: Colors.amber);
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: const Text('HomeView'), centerTitle: true),
       body: SingleChildScrollView(
         child: Column(
-          children: const [
-            SizedBox(height: 50),
+          children: [
+            const SizedBox(height: 50),
             AddressWidget(
               width: 400,
               height: 450,
               color: Colors.blueGrey,
               addressStyle: AddressStyle.twoColumn,
               showChangeStyleIcon: true,
-              title: MyChild(
-                  width: 200, text: 'THIS IS TITLE', color: Colors.amber),
-              child: [
-                MyChild(text: 'Child 1', color: Colors.green),
-                MyChild(text: 'Child 2', color: Colors.blue),
-                MyChild(text: 'Child 3', color: Colors.red),
-                MyChild(text: 'Child 4', color: Colors.yellow),
-                MyChild(text: 'Child 5', color: Colors.pinkAccent),
-                MyChild(text: 'Child 6', color: Colors.lightGreen),
-              ],
+              title: buildTitle(),
+              child: buildChild1(),
             ),
-            SizedBox(height: 20),
+            const SizedBox(height: 20),
             AddressWidget(
-              width: 400,
-              height: 450,
+              width: 240,
+              height: 350,
               color: Colors.blueGrey,
               addressStyle: AddressStyle.twoColumn,
-              showChangeStyleIcon: true,
-              title: MyChild(
-                  width: 200, text: 'THIS IS TITLE', color: Colors.amber),
-              child: [
-                MyChild(text: 'Child 1', color: Colors.green),
-                MyChild(text: 'Child 2', color: Colors.blue),
-                MyChild(text: 'Child 3', color: Colors.red),
-                MyChild(text: 'Child 4', color: Colors.yellow),
-              ],
+              showChangeStyleIcon: false,
+              title: buildTitle(),
+              child: buildChild2(),
             ),
-            SizedBox(height: 200),
+            const SizedBox(height: 200),
           ],
         ),
       ),
